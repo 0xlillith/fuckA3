a865dd04ee93bc5b2cfbea7394a52e85b23455dc
lillith <35337584+0xlillith@users.noreply.github.com>
2024-08-19 13:39:16 +0200

    Update fuckA3.py

--- fuckA3.py
+++ fuckA3.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import requests
-import ffmpeg
+import ffmpeg # Requires only pip install ffmpeg-python, but not ffmpeg
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
